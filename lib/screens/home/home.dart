c1b0179c00ab99c94eb59f7af0a49c8126b397e8
ahmed ragab <ar1755@fayoum.edu.eg>
2023-12-11 20:02:13 +0200

    desgin code structre

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeNote extends StatelessWidget {
+  const HomeNote({super.key});
+  static const String routeName = 'homeNote';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
