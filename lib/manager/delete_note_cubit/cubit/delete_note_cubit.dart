ce22f934b53219f32af8a1f32bcbc3a98a0c12bd
ahmed ragab <ar1755@fayoum.edu.eg>
2023-12-15 14:22:06 +0200

    delete unused filles

--- lib/manager/delete_note_cubit/cubit/delete_note_cubit.dart
+++
@@ -1,7 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:notehive/manager/delete_note_cubit/cubit/delete_note_state.dart';
-
-
-class DeleteNoteCubit extends Cubit<DeleteNoteState> {
-  DeleteNoteCubit() : super(DeleteNoteInitial());
-}
